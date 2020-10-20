f88db4fd5c28963fa20a23a91cf3c645bc81fd00
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2020-10-20 20:25:46 +0530

    Basics in objects

---
+++ object-101.js
@@ -0,0 +1,11 @@
+let myBook = {
+    title:'Ice age',
+    author:'Andrew bake',
+    pageCount:232
+}
+
+console.log(`${myBook.title} is written by ${myBook.author}`);
+
+myBook.author = 'Jacod denly'
+
+console.log(`${myBook.title} is written by ${myBook.author}`);
